323dc22cc94901e311d1f7b0d9a2c20590de6327
Kola Kolade <kolakolade@gmail.com>
2023-04-30 00:18:01 +0100

    Add about section content & styles

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const about = document.querySelector('#about');
+const tabLinks = document.querySelectorAll('.about__tab-link');
+const tabContents = document.querySelectorAll('.about__tab-content');
+const aboutOverlay = document.querySelector('.about__overlay');
+const popup = document.querySelector('.about__popup');
+const popupClose = document.querySelector('.about__popup__close');
+
+about.addEventListener('click', (e) => {
+  if (e.target.classList.contains('about__tab-link')) {
+    tabLinks.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    e.target.classList.add('active');
+
+    tabContents.forEach((content) => {
+      content.classList.remove('active');
+      if (e.target.textContent.toLowerCase() === content.id) {
+        content.classList.add('active');
+      }
+    });
+  }
+});
+
+about.addEventListener('click', (e) => {
+  if (e.target.classList.contains('about__more')) {
+    aboutOverlay.classList.remove('hidden');
+  }
+});
+
+aboutOverlay.addEventListener('click', (e) => {
+  if (e.target === popupClose || e.target !== popup) {
+    aboutOverlay.classList.add('hidden');
+  }
+});
---
+++ src/js/typed.js
@@ -0,0 +1,14 @@
+import Typed from 'typed.js';
+
+let typed = new Typed('.typewriter', {
+  strings: [
+    ,
+    'Full Stack Web Developer',
+    'Mobile App Developer',
+    'Wordpress Developer',
+    'Tech Enthusiast',
+  ],
+  typeSpeed: 60,
+  backSpeed: 100,
+  loop: true,
+});
